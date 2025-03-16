e3ebede157d9465d6f1a0e2edf43021b1eefc7c8
Maryam Elgohary <maryamelgohary4@gmail.com>
2025-03-16 14:38:55 +0200

    app constant colors added

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color darkBrown = Color(0xff493628);
+  static const Color lightBrown = Color(0xffAB886D);
+  static const Color orangeColor = Color(0xffF2A666);
+  static const Color BeigeColor = Color(0xffD6C0B3);
+  static const Color lightBeige = Color(0xffF2E9E4);
+}
